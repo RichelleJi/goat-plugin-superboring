645f8c2d97fc853bcbd7ea1b7875676649d0f252
richelleji <ji.richelle@gmail.com>
2025-03-08 21:45:52 -0700

    lint parameter file

--- typescript/packages/plugins/superboring/src/parameters.ts
+++ typescript/packages/plugins/superboring/src/parameters.ts
@@ -14,7 +14,9 @@ export class GetParamsParameters extends createToolParameters(
         flowRate: z.string().describe("The flow rate in tokens per second"),
         distributor: z.string().describe("The distributor address (use ZeroAddress if not needed)"),
         referrer: z.string().describe("The referrer address (use ZeroAddress if not needed)"),
-        upgradeAmount: z.string().describe("The amount of tokens to upgrade (use MaxUint256 for maximum possible amount)"),
+        upgradeAmount: z
+            .string()
+            .describe("The amount of tokens to upgrade (use MaxUint256 for maximum possible amount)"),
     }),
 ) {}
 
@@ -32,5 +34,3 @@ export class RunMacroParameters extends createToolParameters(
         params: z.string().describe("The encoded parameters for the macro"),
     }),
 ) {}
-
-
